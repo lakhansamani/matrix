96d6400340accd3ca0c4a95e00b99e5becdccd60
lakhansamani <lakhan.m.samani@gmail.com>
2014-05-18 19:46:19 +0530

    Added function to traverse towards south in matrix

--- functions.js
+++ functions.js
@@ -30,3 +30,10 @@ function traverseNorth(m,n,l){
   console.log(result);
   return result;
 }
+
+function traverseSouth(m,n,l){
+  var items=[[1,2,3],[4,5,6],[7,8,9]];
+  var result=(items[m+l][n]);
+  console.log(result);
+  return result;
+}
