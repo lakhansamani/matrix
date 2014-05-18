6aab12b8a832fafc8d2f2ef6c546cd5c9bfc5036
lakhansamani <lakhan.m.samani@gmail.com>
2014-05-18 19:45:51 +0530

    Added function to traverse towards north in matrix

--- functions.js
+++ functions.js
@@ -23,3 +23,10 @@ function traverseWest(m,n,l){
   console.log(result);
   return result;
 }
+
+function traverseNorth(m,n,l){
+  var items=[[1,2,3],[4,5,6],[7,8,9]];
+  var result=(items[m-l][n]);
+  console.log(result);
+  return result;
+}
