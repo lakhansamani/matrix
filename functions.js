bdd2a6ff58255215ea9d1a6b27a792fec53ea4b6
lakhansamani <lakhan.m.samani@gmail.com>
2014-05-18 19:40:58 +0530

    Added function to traverse towards east in matrix

--- functions.js
+++ functions.js
@@ -9,3 +9,10 @@ function matrixCreator(row,column){
   }
   return matrix;
 }
+
+function traverseEast(m,n,l){
+  var items=[[1,2,3],[4,5,6],[7,8,9]];
+  var result=(items[m][n+l]);
+  console.log(result);
+  return result;
+}
